030df7ca66ee7961dae0c629d60647c3b65319b0
Driver <peplinskikrzysztof@gmail.com>
2018-10-07 10:51:05 +0200

    first commit

---
+++ src/main/java/com/example/reactiveprogexample/ReactiveProgExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.reactiveprogexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/reactiveprogexample/ReactiveProgExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.reactiveprogexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveProgExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
